00d7e604ed1c924d2498257545edb450f37b4ed9
ranselorm <ranselorm@outlook.com>
2025-09-14 21:05:39 +0000

    Add user API endpoints and modify home route response

--- src/index.js
+++ src/index.js
@@ -3,9 +3,35 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const users = [
+  { id: 1, userName: "randy", displayName: "Ransel" },
+  { id: 2, userName: "jack", displayName: "Jack" },
+  { id: 3, userName: "shannel", displayName: "Shannel" },
+];
+
 app.get("/", (req, res) => {
   console.log("Home page");
-  res.end("<h1>Hello</h1>");
+});
+
+app.get("/api/users", (req, res) => {
+  res.status(201).send(users);
+});
+
+app.get("/api/users/:id", async (req, res) => {
+  const parseId = parseInt(req.params.id);
+
+  console.log(parseId);
+
+  if (isNaN(parseId)) return res.status(400).send({ message: "Bad Request" });
+
+  const user = users.find((item) => item.id === parseId);
+
+  if (!user)
+    return res
+      .status(404)
+      .send({ message: `User with the id ${parseId} not found!` });
+
+  res.status(201).send({ msg: "User with id found successfully", user });
 });
 
 app.listen(PORT, () => {
